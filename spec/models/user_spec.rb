927545127fe268bdae846c73c71374f947a87e96
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-18 11:00:36 -0700

    Initial file add; three green specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe User do
+
+  it { should validate_confirmation_of(:password) }
+
+  describe '#encrypt_password' do
+    it('Creates a salt for passwords when creating a new user') do
+      test_user = User.create({:name => "Andy", :password => "1234"})
+      expect(test_user.password_salt).to be_truthy
+    end
+
+    it('Creates a hash for passwords when creating a new user') do
+      test_user = User.create({:name => "Andy", :password => "1234"})
+      expect(test_user.password_hash).to be_truthy
+    end
+  end
+
+
+
+end
