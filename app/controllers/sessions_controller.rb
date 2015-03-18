4f3b85df82b59041c2737c6dcc2f055d00a52081
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-18 15:32:48 -0700

    Add user signup, signin, and authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    @user = User.authenticate(params[:name], params[:password])
+    if @user
+      flash[:notice] = "Thumbs up!"
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      flash[:alert] = "Thumbs down! There was a problem logging you in"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logout successful!"
+    redirect_to "/"
+  end
+
+end
