7584f8c44295b4f9732d7e0aa91371b763dcd7a4
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-19 11:45:56 -0700

    Add RESTful photo controller actions and a really sweet embedded route

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,19 @@
 class PhotosController < ApplicationController
   def create
-    @photo = Photo.create(photo_params)
+    @photo = Photo.new(photo_params)
     @photo.users << current_user
+    if @photo.save
+      flash[:notice] = "Thuccess!"
+      redirect users_path(current_user)
+    else
+      flash[:alert] = "Thumbs down for thumb reason"
+      render :new
+    end
+  end
+
+  def new
+    @photo = Photo.new
+    render :new
   end
 
   private
